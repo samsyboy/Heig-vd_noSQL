9fab50f67b7e0fe3f352a241df27b163cc029f01
samsyboy <samuel.teixeira1@hes-so.ch>
2024-10-10 11:19:26 +0200

    initialisation du fichier .git

---
+++ src/views/SamuelView.vue
@@ -0,0 +1,53 @@
+<script lang="ts">
+import PouchDB from 'pouchdb'
+
+declare interface Post {
+  _id: string
+  doc: {
+    nom_famille: string
+    description: int
+    details: {
+      date_creation: string
+      auteur: string
+    }
+  }
+}
+
+export default {
+  data() {
+    return {
+      datas: [],
+      databaseReference: null
+    }
+  },
+
+  methods: {
+    inc() {
+      // old
+      // this.total++;
+    },
+
+    initDatabase() {
+      const db = new PouchDB('http://localhost:5984/cours3')
+      if (db) {
+        console.log("Connected to collection 'post'")
+      } else {
+        console.warn('Something went wrong')
+      }
+      this.storage = db
+    },
+
+    fetchData() {}
+  },
+
+  mounted() {
+    this.initDatabase()
+  }
+}
+</script>
+
+<template>
+  <h1>InfraDon2</h1>
+  <p>Counter: {{ datas }}</p>
+  <button type="button" role="button" @click="inc">+1</button>
+</template>
